d0e9aeeb4400663abb8e2add301db0463e59990a
Chris Hong <hongchris97@gmail.com>
2023-01-02 01:40:44 -0800

    Changed offset

--- chris-portfolio/src/components/Navbar.js
+++ chris-portfolio/src/components/Navbar.js
@@ -17,32 +17,32 @@ class Navbar extends Component {
                 <Link 
                 className='navbar-brand col-3 nav-link'
                 activeClass='active' to='homepage' spy={true}
-                smooth={true} offset={-70} duration={500}>
+                smooth={true} offset={0} duration={500}>
                     Chris Hong
                 </Link>
 
                 <Link 
                 className='nav-link'
                 activeClass='active' to='about' spy={true}
-                smooth={true} offset={-70} duration={500}>
+                smooth={true} offset={0} duration={500}>
                     About
                 </Link>
                 <Link 
                 className='nav-link'
                 activeClass='active' to='projects' spy={true}
-                smooth={true} offset={-70} duration={500}>
+                smooth={true} offset={0} duration={500}>
                     Projects
                 </Link>
                 <Link 
                 className='nav-link'
                 activeClass='active' to='resume' spy={true}
-                smooth={true} offset={-70} duration={500}>
+                smooth={true} offset={0} duration={500}>
                     Resume
                 </Link>
                 <Link 
                 className='nav-link'
                 activeClass='active' to='contact' spy={true}
-                smooth={true} offset={-70} duration={500}>
+                smooth={true} offset={-750} duration={500}>
                     Contact
                 </Link>
             
