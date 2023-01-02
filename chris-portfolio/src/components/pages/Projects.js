e31701344d1c32c5e9263e5eb3f97da3494ee149
Chris Hong <hongchris97@gmail.com>
2023-01-01 19:32:44 -0800

    Description buttons display correct modal onclick.

--- chris-portfolio/src/components/pages/Projects.js
+++ chris-portfolio/src/components/pages/Projects.js
@@ -1,14 +1,21 @@
-import React, {useState} from "react";
+import {React, useState} from "react";
 import {  FaGithubAlt } from 'react-icons/fa'
-import { Modal } from 'react-bootstrap';
 import { ghprojects } from "../../data"
 import '../styles/index.css'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Projects() {
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  
+  const [show, setShow] = useState({});
+
+  const handleClose = (id) => {
+    setShow((prevState) => ({ ...prevState, [id]: false }));
+  };
+
+  const handleShow = (id) => {
+    setShow((prevState) => ({ ...prevState, [id]: true }));
+  };
+
   return (
       <section id="projects" className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-10 mx-auto text-center lg:px-40 col-12">
@@ -17,44 +24,49 @@ export default function Projects() {
           </h1>
 
         <div className="flex flex-wrap m-1 row">
-          {ghprojects.map((project) => (
-            <div className="col-lg-6" style={{marginBottom:'5%', marginTop:'5%'}}>
-              <h2 className="mb-3 col-12" key={project.title}>
-                {project.title}
+
+          {ghprojects.map((item) => (
+            <div className="col-lg-6" style={{marginBottom:'5%', marginTop:'5%'}} key={item.id}>
+              <h2 className="mb-3 col-12" key={item.title} value={item.title}>
+                {item.title}
               </h2>
               <div className="flex relative" id='portfolioSquares'>
-                  <a key={project.link} href={project.link} rel='noeferrer' target='__blank'><img 
-                    key={project.image}
-                    alt="gallery"
-                    className="col-12 m-4"
-                    src={project.image}
-                    style={{height:'300px', opacity:'.85'}}
-                  /></a>
+                  <a key={item.link} href={item.link} rel='noeferrer' target='__blank'>
+                    <img 
+                      key={item.image}
+                      alt="gallery"
+                      className="col-12 m-4"
+                      src={item.image}
+                      style={{height:'300px', opacity:'.85'}}
+                    />
+                  </a>
               </div> 
               
               <div 
                 className='d-flex flex-row justify-content-center'>
                   <div className="col-12" style={{display:'flex', textAlign:'center', justifyContent:'center', alignItems:'center'}}> 
-                    <a key={project.github} href={project.github} rel="noreferrer"
+                    <a key={item.github} href={item.github} rel="noreferrer"
                     target="_blank" className='col-7'>
                       <FaGithubAlt size={25}/>
                     </a>
-                    <button type='button' 
-                    className='btn btn-dark col' id='description'
-                    variant="primary" onClick={handleShow} key={project.subtitle} value={project.subtitle}
-                    >Description 
+                    
+                    <button type='button' className='btn btn-dark col' id='description' 
+                    onClick={() => handleShow(item.id)}>
+                        Description
                     </button>
 
-                    {ghprojects.map((project) => (
-                      <Modal show={show} onHide={handleClose}>                      
-                        <Modal.Header closeButton>
-                          <Modal.Title key={project.title}>{project.title}</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body key={project.subtitle}>{project.subtitle}</Modal.Body>
-                        <Modal.Footer>
-                        </Modal.Footer>
-                      </Modal>
-                   ))}
+                    <Modal id={item.id} show={show[item.id]} onHide={() => handleClose(item.id)}>
+                      <Modal.Header closeButton>
+                        <Modal.Title>{item.title}</Modal.Title>
+                      </Modal.Header>
+                      <Modal.Body>{item.subtitle}</Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={() => handleClose(item.id)}>
+                          Close
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+
                   </div>
               </div>
             </div>
