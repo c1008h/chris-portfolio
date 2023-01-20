cf76a922cb24d57d71cdca2ee60396a715af8deb
Chris Hong <hongchris97@gmail.com>
2023-01-19 18:06:14 -0800

    Update data.js

--- chris-portfolio/src/data.js
+++ chris-portfolio/src/data.js
@@ -1,53 +1,69 @@
 export const ghprojects = [
-  {
-    title: "DogHub",
-    subtitle: "Handlebars & REST",
-    description:
-      "An app that is used to find your dog a suitable friend.",
-    image: "./images/doghub.png",
-    github:'https://github.com/davidpascual2/Project-2',
-    link: "https://ancient-scrubland-48723.herokuapp.com/",
-  },
-  {
-    title: "Fork The Food",
-    subtitle: "JavaScript, HTML, and CSS",
-    description:
-      "This application is a tool for taking the ingredients a user inputs and returning a recipe that utilizes those ingredients. In addition to this a user can save their favorite recipes to local storage to be accessed later.",
-    image: "./images/forkthefood.png",
-    github:'https://github.com/EyesackEl/fork-the-food',
-    link: "https://eyesackel.github.io/fork-the-food/",
-  },
-  {
-    title: "Bricks Repo",
-    subtitle: "Full stack MERN",
-    description:
-      "An app that allows users to be nolstalgic about old lego sets or learn more about new lego sets.",
-    image: "./images/bricksrepo.png",
-    github:'https://github.com/JimMason564/Project3',
-    link: "https://bricks-repo.herokuapp.com/",
-  },
-  {
-    title: "E-Commerance",
-    subtitle: "Node, MySql & Express",
-    description:
-      "A backend of an e-commerce store that is used to store data from categories, products, and tags",
-    image: "./images/ecombackend.png",
-    github:'https://github.com/c1008h/E-Commerce-Backend',
-  },
-  {
-    title: "Note Taker",
-    subtitle: "JavaScript, Express, and Node.js",
-    description:
-      "An application that allows users to add notes, edit notes, and delete notes",
-    image: "./images/notetaker.png",
-    github:'https://github.com/c1008h/Note-Taker',
-  },
-  {
-    title: "Employee Profile",
-    subtitle: "JavaScript, Node.js & Express",
-    description:
-      "An application to keep track of employees within a company by answering questions in the terminal and render it onto the web",
-    image: "./images/teamprofilegenerator.png",
-    github:'https://github.com/c1008h/team-profile-generator',
-  }
+    {
+        id: 1,
+        title: "DogHub",
+        subtitle: "Handlebars & REST",
+        description:
+        "An app that is used to find your dog a suitable friend.",
+        image: "./images/doghub.png",
+        github:'https://github.com/davidpascual2/Project-2',
+        link: "https://ancient-scrubland-48723.herokuapp.com/",
+    },
+    {
+        id: 2,
+        title: "Fork The Food",
+        subtitle: "JavaScript, HTML, and CSS",
+        description:
+        "This application is a tool for taking the ingredients a user inputs and returning a recipe that utilizes those ingredients. In addition to this a user can save their favorite recipes to local storage to be accessed later.",
+        image: "./images/forkthefood.png",
+        github:'https://github.com/EyesackEl/fork-the-food',
+        link: "https://eyesackel.github.io/fork-the-food/",
+    },
+    {
+        id: 3,
+        title: "Bricks Repo",
+        subtitle: "Full stack MERN",
+        description:
+        "An app that allows users to be nolstalgic about old lego sets or learn more about new lego sets.",
+        image: "./images/bricksrepo.png",
+        github:'https://github.com/JimMason564/Project3',
+        link: "https://bricks-repo.herokuapp.com/",
+    },
+    {
+        id: 4,
+        title: "E-Commerance",
+        subtitle: "Node, MySql & Express",
+        description:
+        "A backend of an e-commerce store that is used to store data from categories, products, and tags",
+        image: "./images/ecombackend.png",
+        github:'https://github.com/c1008h/E-Commerce-Backend',
+    },
+    {
+        id: 5,
+        title: "Note Taker",
+        subtitle: "JavaScript, Express, and Node.js",
+        description:
+        "An application that allows users to add notes, edit notes, and delete notes",
+        image: "./images/notetaker.png",
+        github:'https://github.com/c1008h/Note-Taker',
+    },
+    {
+        id: 6,
+        title: "Employee Profile",
+        subtitle: "JavaScript, Node.js & Express",
+        description:
+        "An application to keep track of employees within a company by answering questions in the terminal and render it onto the web",
+        image: "./images/teamprofilegenerator.png",
+        github:'https://github.com/c1008h/team-profile-generator',
+    },
+    {
+        id: 7,
+        title: "Day Planner",
+        subtitle: "JavaScript, jQuery, CSS, Third Party APIs & Bootstrap",
+        description:
+        "This app utilizes third-party APIs to allow users to input their schedule between the hours of 9am and 5pm. The background color of the schedule will change dynamically to indicate whether a specific time has passed, is currently happening, or is yet to come. This feature allows users to easily keep track of their schedule and stay on top of their tasks throughout the day.",
+        image: "./images/workday",
+        github:'https://github.com/c1008h/team-profile-generator',
+        link: "https://c1008h.github.io/Day-Planner/"
+    },
 ];
