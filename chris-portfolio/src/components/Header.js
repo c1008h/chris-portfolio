def2aa500b8f9de0c1f013e70c8b9bc00cdb9d84
Chris Hong <hongchristinaaa@gmail.com>
2023-02-05 00:03:26 -0800

    animated name on homepage

--- chris-portfolio/src/components/Header.js
+++ chris-portfolio/src/components/Header.js
@@ -1,21 +1,50 @@
 import React, {useState, useEffect} from 'react';
 import {FaArrowDown} from 'react-icons/fa'
+import {Button} from 'react-bootstrap'
 import { Link } from 'react-scroll';
 import '../styles/header.css'
 
 export default function Header() {
-  return (
+  const [letters, setLetters] = useState([]);
+  const name = "CHRIS HONG";
+
+  useEffect(() => {
+    let timeoutId;
+    const animateLetters = (index) => {
+      timeoutId = setTimeout(() => {
+        setLetters((prevLetters) => [...prevLetters, name[index]]);
+        if (index < name.length - 1) {
+          animateLetters(index + 1);
+        }
+      }, 500);
+    };
+    animateLetters(0);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+
+    return (
     
     <header id='homepage' className='col-12'>
       <div id='home'>
-        <pre className='responsive-headerline' id='name'>
-          <code>
-            &lt;h1&gt; Hi, my name is Chris! &lt;/h1&gt;
-          </code>
-        </pre>
+        <div className='name-container'>
+          <h1 className="name">
+            {letters.map((letter, index) => (
+            <span key={index} className="letter">
+              {letter}
+            </span>
+            ))}
+          </h1>
+        </div>
+
         <Link activeClass='active' to='about' spy={true}
           smooth={true} offset={0} duration={500}>
-          <FaArrowDown id='downBtn'/>
+          {/* <FaArrowDown id='downBtn'/> */}
+          <Button id='downBtn'>More Info</Button>
+
         </Link>
       </div>
     </header>
