78291bf8538a9bb295cd0f52f846ee2dc16a297f
Chris Hong <hongchris97@gmail.com>
2023-03-06 00:18:29 -0800

    updating modal demo

--- chris-portfolio/src/components/projects/Modal.js
+++ chris-portfolio/src/components/projects/Modal.js
@@ -4,23 +4,6 @@ import Modal from 'react-bootstrap/Modal';
 
 export default function ProjectModal({handleClose, item, show}) {
     return (
-        // <Modal id={props.item.id} show={props.show[props.item.id]} onHide={() => props.handleClose(props.item.id)}>
-        //     <Modal.Header closeButton>
-        //         <Modal.Title className="col-5">{props.item.title}</Modal.Title>
-        //         <p className="col-3" style={{}}>{props.item.subtitle}</p>
-        //     </Modal.Header>
-        //     <Modal.Body style={{margin:'2px'}}>
-        //         <img src={props.item.demo} style={{width:'85%', justifyContent:'center', borderRadius:'25px'}} alt='short demo of app'/>
-        //         <br/>
-        //         {props.item.description}
-        //     </Modal.Body>
-        //     <Modal.Footer>
-        //         <Button variant="secondary" onClick={() => props.handleClose(props.item.id)}>
-        //             Close
-        //         </Button>
-        //     </Modal.Footer>
-        // </Modal>
-
         <Modal key={item} id={item.id} show={show[item.id]} onHide={() => handleClose(item.id)}>
             <Modal.Header closeButton>
                 <Modal.Title className="col-5" key={item.title}>{item.title}</Modal.Title>
@@ -28,7 +11,7 @@ export default function ProjectModal({handleClose, item, show}) {
             </Modal.Header>
             <Modal.Body style={{margin:'2px'}}>
                 <div style={{textAlign:'center'}}>
-                <img src={item.demo} style={{width:'85%', justifyContent:'center', borderRadius:'15px'}} alt='short demo of app'/>
+                <img src={item.demo} style={{width:'85%', height: '50%', justifyContent:'center', borderRadius:'15px'}} alt='short demo of app'/>
                     <br/>
                     {item.description}
                 </div>
