48b41a2b4561da4fd7712f4cf14e68a7cdfb264e
Chris Hong <hongchris97@gmail.com>
2023-03-05 23:58:59 -0800

    Added loading page and updated bio

--- chris-portfolio/src/components/Navbar.js
+++ chris-portfolio/src/components/Navbar.js
@@ -13,17 +13,7 @@ export default function Navbar () {
         <>
        
         <nav id="navbar"
-        // className='navbar col-12 navbar-expand-lg navbar-light bg-light'
         className={className}
-        // style={{
-        //     // display: show ? "transparent" : "none",
-        //     // height: '90px', 
-        //     // justifyContent:'space-around', 
-        //     // position:'fixed', 
-        //     // top:'-90px',
-        //     // zIndex:'1',
-        //     // transition: "top 0.3s"
-        //     }}
         >
             <Link 
             className='navbar-brand col-3 nav-link'
@@ -63,6 +53,7 @@ export default function Navbar () {
             smooth={true} offset={-750} duration={500}>
                 Contact
             </Link>
+            <button><a href='https://drive.google.com/file/d/157zT_h-jEf0DpMbv1Zs64yn-oVH1MZlQ/view?usp=sharing' target='__blank'>Resume</a></button>
         
         </nav> 
       
