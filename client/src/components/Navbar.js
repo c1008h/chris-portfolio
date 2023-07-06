514c9744d0436abb0162ef662d76c375a5e57852
Chris Hong <hongchris97@gmail.com>
2023-07-06 01:08:58 -0700

    updated resume link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar () {
                 Contact
             </Link>
             <button id='resumeBtn'>
-                <a href='https://drive.google.com/file/d/1zH23MOrQsAYbVNPZ2Kvgh5Yc6sP8M-lw/view?usp=sharing' 
+                <a href='https://drive.google.com/file/d/1XbMgtUqrWV5rA4phXNIYLvIbiDA3QScf/view?usp=sharing' 
                 target='_blank'
                 rel='noreferrer'
                 id='resume'
