c272927f35f005abf03d27fe6881cb0b7f273455
Chris Hong <hongchris97@gmail.com>
2023-07-05 23:47:42 -0700

    deploying to netlify

--- client/src/pages/Experience.js
+++ client/src/pages/Experience.js
@@ -6,6 +6,7 @@ import '../styles/experience.css'
 export default function Experience() {
     const [activeNavItem, setActiveNavItem] = useState(new Date().getFullYear().toString());
     const currentYear = new Date().getFullYear().toString();
+  // eslint-disable-next-line no-unused-vars
 
     const [expandedCards, setExpandedCards] = useState([]);
 
