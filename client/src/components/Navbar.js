ab5a452a03cf8001e4eb78228410f92c658ca45f
Chris Hong <hongchris97@gmail.com>
2023-07-06 19:01:17 -0700

    Updated link

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -45,7 +45,7 @@ export default function Navbar () {
                 Contact
             </Link>
             <button id='resumeBtn'>
-                <a href='https://drive.google.com/file/d/1XbMgtUqrWV5rA4phXNIYLvIbiDA3QScf/view?usp=sharing' 
+                <a href='https://drive.google.com/file/d/1xVN18gLXjKLJzzQVDb97yWpMI6ui7Erp/view?usp=sharing' 
                 target='_blank'
                 rel='noreferrer'
                 id='resume'
