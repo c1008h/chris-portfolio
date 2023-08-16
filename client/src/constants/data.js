ef78a5098199e4ae93a475d70a30323fb395722c
Chris Hong <hongchris97@gmail.com>
2023-08-16 03:43:17 -0700

    added pefi description

--- client/src/constants/data.js
+++ client/src/constants/data.js
@@ -15,8 +15,7 @@ export const ghprojects = [
         title: "Pefi",
         subtitle: "MongoDB, Express, React, Node.Js, GraphQL",
         description:
-        "This application is a tool for taking the ingredients a user inputs and returning a recipe that utilizes those ingredients. In addition to this a user can save their favorite recipes to local storage to be accessed later.",
-        image: "../images/pefi-dash.png",
+        "Pefi is a comprehensive personal finance management app that empowers users to plan, track, and achieve their financial goals. With intuitive interfaces, interactive charts, and powerful tools, Pefi helps users make informed decisions about their money.",
         github:'https://github.com/c1008h/Pefi',
         // link: "https://eyesackel.github.io/fork-the-food/",
         demo: '../demo/PeFi.gif'
