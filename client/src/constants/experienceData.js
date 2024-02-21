915e0828d753222d845e33e4bcce6b405a0061e2
Chris Hong <hongchris97@gmail.com>
2024-02-20 23:44:08 -0800

    updated job and job descriptions

--- client/src/constants/experienceData.js
+++ client/src/constants/experienceData.js
@@ -1,11 +1,27 @@
 export const experienceData = [
     {
+        id: 11,
+        year:'2024',
+        yearRange: '2024-Present',
+        name: 'Full Stack Engineer',
+        institute: 'Self Employed',
+        description: 'Collaborate with clients to design, develop, and deploy web applications. Create responsive front-end interfaces and robust back-end solutions. Proficient in HTML, CSS, JavaScript, Node.js, Express.js, and integrating APIs. Successfully completed multiple projects on time and within budget, ensuring client satisfaction.'
+    },
+    {
+        id: 10,
+        year:'2024',
+        yearRange: '2023-Present',
+        name: 'Software Engineer',
+        institute: 'Reflectica AI',
+        description: 'Lead developer at Reflectica AI, enhancing psychotherapy platform with React Native and Firebase. Achieved 20% improvement in application responsiveness. Developed full-stack web app with Node.js, RESTful APIs, and Redux. Managed product lifecycle, troubleshooting, and maintaining high-quality standards.'
+    },
+    {
         id: 9,
         year:'2023',
-        yearRange: '2023-Present',
+        yearRange: '2023-2024',
         name: 'Software Engineer Intern',
         institute: 'Reflectica AI',
-        description:'As a Software Engineer Intern at Reflectica AI in Seattle, WA from 2023 to the present, I have had the privilege to work alongside a dynamic and diverse engineering team. Together, we embarked on the journey of conceptualizing, designing, and implementing cutting-edge features for Reflectica\'s hyper-realistic audio therapy platform. During this internship, I devoted significant efforts to meticulous testing, ensuring that our software operates at peak performance and maintains the highest standards of reliability. Additionally, I actively participated in code reviews, fostering a culture of excellence by providing and receiving constructive feedback, a practice that greatly enriches teamwork and code quality. As part of my role, I also resolved software-related issues, underscoring my robust problem-solving abilities and contributing to the platform\'s overall stability and functionality. This internship has been an invaluable learning experience, allowing me to hone my skills as a software engineer and make a meaningful impact in a collaborative, innovative environment.' 
+        description: 'Contributed to Reflectica AI\'s psychotherapy platform development. Conducted thorough testing, participated in code reviews, and resolved software issues. Collaborated effectively with engineering team, ensuring platform stability and functionality.'
     },
     {
         id: 8,
@@ -13,15 +29,15 @@ export const experienceData = [
         yearRange: '2023-Present',
         name: 'Coding Instructor',
         institute: 'CodeRev Kids',
-        description:'In my role as a STEM/STEAM After School Instructor at CodeREV Kids, I had the privilege of equipping students with essential Programming Fundamentals and Video Game Development skills, leveraging tools like Pencil Code and Scratch. These experiences fostered their creativity and problem-solving abilities, providing them with a solid foundation in STEAM subjects. Additionally, I introduced hands-on learning through Arduino for Snap Circuits Engineering, allowing students to explore electronics and circuitry in a practical way. I also played a key role in enhancing students\' coding proficiency and problem-solving skills through expert debugging and guidance. This experience allowed me to develop valuable skills in problem-solving, Scratch, Arduino, debugging, programming, technical training, technical leadership, public speaking, and effective communication.' 
+        description: 'Taught programming fundamentals and video game development to students. Utilized tools like Pencil Code and Scratch. Introduced hands-on learning with Arduino for Snap Circuits Engineering. Enhanced students\' coding proficiency through expert guidance.'
     },
     {
         id: 7,
         year:'2023',
-        yearRange: '2023-Present',
+        yearRange: '2023-2024',
         name: 'Learning Assistant',
         institute: 'edX',
-        description:'As a Learning Assistant, I provided exceptional support to students by troubleshooting technical issues and answering questions related to various programming languages and concepts, including HTML, CSS, JavaScript, React, jQuery, Handlebars, Node, Express, SQL, NoSQL, MySQL, GraphQL, GitHub, REST API, and computer science fundamentals. Through my guidance and mentoring, I helped students develop their collaboration, communication, and problem-solving skills while adhering to professional practices and industry standards. In a fast-paced and immersive coding environment, I adapted my approach to meet student needs, maintaining an up-to-date understanding of the bootcamp curriculum and learning objectives. Additionally, I fostered a supportive and inclusive learning community in a virtual environment, utilizing my interpersonal skills and emotional intelligence to build trust and engagement.' 
+        description: 'Provided exceptional support to students in various programming languages and concepts. Fostered collaborative learning environment and adapted teaching methods to meet student needs. Built trust and engagement in virtual learning community.'
     },
     {
         id: 6,
@@ -29,7 +45,7 @@ export const experienceData = [
         yearRange: '2023-Present',
         name: 'Founder and Administrator',
         institute: 'Programming Accountability Group',
-        description:'As the founder and admin of a programming accountability group, I took on the responsibility of organizing regular meetings, setting goals and guidelines, and providing feedback and support to help developers stay motivated and accountable. This experience allowed me to develop important skills in leadership, time management, and collaboration, while also contributing to the growth and success of the group. It was a rewarding experience that helped me refine my professional and entrepreneurial skills.' 
+        description: 'Founded and administered programming accountability group. Organized meetings, set goals, and provided support to members. Developed leadership and collaboration skills.'
     },
     {
         id: 5,
@@ -37,7 +53,7 @@ export const experienceData = [
         yearRange: '2023-2023',
         name: 'Hackathon',
         institute: 'Bay Area Hacks 23',
-        description:'During this hackathon event, I worked on a team of two to develop a MERN application with GraphQL and Material UI. Originally a team of four, we had to adapt to unexpected situations when some members bailed, but we remained focused on completing the project within the given timeframe of less than 48 hours. As a full-stack developer, I was responsible for both the frontend and backend of the application, utilizing React, JavaScript, Express, Node, and GraphQL. Our project was a job search application that utilized a job API and MongoDB as our database. We also delivered a pitch presentation to showcase our product and its features. This experience not only enhanced my skills in software development but also strengthened my ability to work under pressure and adapt to unexpected changes.' 
+        description: 'Developed MERN application with GraphQL and Material UI. Managed frontend and backend using React, Express, Node, and MongoDB. Delivered pitch presentation under time pressure.'
     },
     {
         id: 4,
@@ -45,7 +61,7 @@ export const experienceData = [
         yearRange: '2022-2023',
         name: 'Leasing Consultant',
         institute: 'Asset Living',
-        description:'I successfully increased social media interaction by 22.7% on Tiktok and Instagram by creating engaging content and contributing creative marketing ideas. I also played a key role in boosting the business\'s Google rating from 1.2-star to 3.1-star in just 3 months by effectively managing relationships with clients and being attentive to their needs. In addition, I effectively managed a successful move-out and move-in process of over 70% of 750+ bed spaces within a time constraint of 17 days as part of a team. I advised prospects and clients in signing lease agreements by building rapport and adapting to unexpected situations. Moreover, I assisted residents with navigating and troubleshooting the resident portal, resulting in a reduction of customer service inquiries and increased satisfaction. Through these experiences, I gained valuable skills in customer service, project management, and effective communication.' 
+        description: 'Increased social media interaction by 22.7% on TikTok and Instagram. Boosted Google rating from 1.2 to 3.1 stars. Managed move-in/move-out process for 70% of 750+ bed spaces. Provided exceptional customer service and resolved inquiries.'
     },
     {
         id: 3,
@@ -53,7 +69,7 @@ export const experienceData = [
         yearRange: '2022-2022',
         name: 'Fullstack Coding Bootcamp',
         institute: 'University of Washington',
-        description:'I completed a fast-paced, 24-week full-stack coding bootcamp where I gained hands-on experience with HTML, CSS, JavaScript, third-party APIs, Git, Agile methodology, and more. I also learned about server-side APIs, OOP, and SQL, as well as worked with relational and NoSQL databases, and gained a deep understanding of computer science fundamentals such as algorithms, data structures, and Big O notation. I collaborated on various projects, including group projects, and gained experience with PWA performance, React, and complex state management with Redux and the Context API. Overall, I am confident in my ability to build dynamic, responsive web applications.' 
+        description: 'Completed intensive 24-week full-stack coding bootcamp. Proficient in HTML, CSS, JavaScript, Git, and Agile methodology. Developed skills in server-side APIs, OOP, SQL, and React.'
     },
     {
         id: 2,
@@ -61,7 +77,7 @@ export const experienceData = [
         yearRange: '2021-2022',
         name: 'Courier',
         institute: 'DoorDash/UberEats',
-        description: 'As a courier, I honed my time management, communication, and problem-solving skills. I maintained a 99% satisfaction rate by consulting with vendors and clients to understand their needs and expectations. I successfully completed all deliveries within the allotted time frame, efficiently adapting to changing situations such as traffic or weather. Through careful analysis of high traffic hours and efficient routes, I curated a plan to reach my daily goals. Additionally, I managed my finances as a business owner to maximize profit. This experience taught me the importance of attention to detail, reliability, and professionalism in all aspects of my work.'
+        description: 'Delivered orders on time with 99% satisfaction rate. Managed routes efficiently and maximized profit. Demonstrated reliability, professionalism, and problem-solving skills.'
     },
     {
         id: 1,
@@ -69,6 +85,6 @@ export const experienceData = [
         yearRange: '2016-2020',
         name: 'Bachelor\'s Degree',
         institute: 'University of Washington',
-        description: 'As a BA graduate in Anthropology, I completed a rigorous curriculum of core social science courses, including psychology and sociology, as well as STEM courses in math, biology, chemistry, and physics. My passion for technology led me to pursue my interest in informatics, where I gained hands-on experience with CSS, HTML, and SEO, and learned about website flow and information technology. Through these experiences, I honed my critical thinking and analytical skills, as well as my ability to work independently and collaborate effectively with others.'
+        description: 'Graduated with BA in Anthropology. Gained skills in critical thinking, analysis, and collaboration. Developed interest in informatics and technology.'
     }
 ]
